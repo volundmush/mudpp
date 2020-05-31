6a4205caf1fb28f4431319362bfa5667cfd1bcf9
Andrew Bastien <volundmush@gmail.com>
2020-05-30 21:15:43 -0700

    Telnet framework is starting to look good.

--- telnet.cpp
+++ telnet.cpp
@@ -13,8 +13,129 @@ mudpp::net::Protocol *mudpp::net::telnet::TelnetProtocolHandler::WrapConnection(
 }
 
 void mudpp::net::telnet::TelnetProtocol::Receive(mudpp::net::bytes data) {
-    // Just gonna echo for the moment.
+
+    for (auto &byte : data)
+    {
+        switch(this->state)
+        {
+            case DATA:
+                switch(byte)
+                {
+                    case telnet_codes::IAC:
+                        this->state = ESCAPED;
+                        break;
+                    case telnet_codes::CR:
+                        this->state = ENDLINE;
+                        break;
+                    default:
+                        this->user_command_buffer.push_back(byte);
+                        break;
+                }
+                break;
+            case ESCAPED:
+                switch(byte)
+                {
+                    case telnet_codes::WILL:
+                        this->state = COMMAND;
+                        this->command_mode = telnet_codes::WILL;
+                        break;
+                    case telnet_codes::WONT:
+                        this->state = COMMAND;
+                        this->command_mode = telnet_codes::WONT;
+                        break;
+                    case telnet_codes::DO:
+                        this->state = COMMAND;
+                        this->command_mode = telnet_codes::DO;
+                        break;
+                    case telnet_codes::DONT:
+                        this->state = COMMAND;
+                        this->command_mode = telnet_codes::DONT;
+                        break;
+                    case telnet_codes::SB:
+                        this->state = SUBNEGOTIATION;
+                        break;
+                    case telnet_codes::IAC:
+                        this->state = DATA;
+                        this->user_command_buffer.push_back(byte);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case COMMAND:
+                this->ProcessCommand(this->command_mode, byte);
+                this->state = DATA;
+                break;
+            case SUBNEGOTIATION:
+                this->subcommand = byte;
+                this->state = IN_SUBNEGOTIATION;
+                break;
+            case IN_SUBNEGOTIATION:
+                switch(byte)
+                {
+                    case telnet_codes::IAC:
+                        this->state = SUB_ESCAPED;
+                        break;
+                    default:
+                        this->sub_command_buffer.push_back(byte);
+                        break;
+                }
+                break;
+            case SUB_ESCAPED:
+                switch(byte)
+                {
+                    case telnet_codes::IAC:
+                        this->state = IN_SUBNEGOTIATION;
+                        this->sub_command_buffer.push_back(byte);
+                        break;
+                    case telnet_codes::SE:
+                        this->state = DATA;
+                        this->ProcessSubCommand(this->subcommand, this->sub_command_buffer);
+                        this->sub_command_buffer.clear();
+                        this->subcommand = 0;
+                    default:
+                        break;
+                }
+                break;
+
+            case ENDLINE:
+                switch(byte)
+                {
+                    case telnet_codes::LF:
+                        this->state = DATA;
+                        this->ProcessUserCommand(this->user_command_buffer);
+                        this->user_command_buffer.clear();
+                        break;
+                    case telnet_codes::IAC:
+                        this->state = DATA;
+                        this->ProcessUserCommand(this->user_command_buffer);
+                        this->user_command_buffer.clear();
+                        break;
+                    default:
+                        this->state = DATA;
+                        this->user_command_buffer.push_back(telnet_codes::CR);
+                        break;
+                }
+                break;
+        }
+    }
+
+}
+
+void mudpp::net::telnet::TelnetProtocol::ProcessCommand(uint8_t command, uint8_t arg) {
+    Send(bytes{'I', 'A', 'C', ' ', command, ' ', arg, CR, LF});
+}
+
+void mudpp::net::telnet::TelnetProtocol::ProcessSubCommand(uint8_t arg, mudpp::net::bytes data) {
+    Send(bytes{'I', 'A', 'C', ' ', 'S', 'B', ' ', arg});
     Send(data);
+    Send(bytes{'I', 'A', 'C', ' ', 'S', 'E', CR, LF});
+}
+
+void mudpp::net::telnet::TelnetProtocol::ProcessUserCommand(mudpp::net::bytes data) {
+    Send(bytes{'R', 'E', 'C', 'V', ' '});
+    Send(data);
+    Send(bytes{CR, LF});
 }
 
 
--- telnet.h
+++ telnet.h
@@ -9,10 +9,79 @@
 
 namespace mudpp::net::telnet {
 
+    enum telnet_codes : uint8_t
+    {
+        NUL = 0,
+        BEL = 7,
+        CR = 13,
+        LF = 10,
+        SGA = 3,
+        NAWS = 31,
+        SE = 240,
+        NOP = 241,
+        GA = 249,
+        SB = 250,
+        WILL = 251,
+        WONT = 252,
+        DO = 253,
+        DONT = 254,
+        IAC = 255,
+
+        // The following are special MUD specific protocols.
+
+        // MUD eXtension Protocol
+        MXP = 91,
+
+        // Mud Server Status Protocol
+        MSSP = 70,
+
+        // Compression
+        // MCCP1 = 85 - this is deprecrated
+        MCCP2 = 86,
+        MCCP3 = 87,
+
+        // GMCP - Generic Mud Communication Protocol
+        GMCP = 201,
+
+        // MSDP - Mud Server Data Protocol
+        MSDP = 69,
+
+        // TTYPE - Terminal Type
+        TTYPE = 24
+    };
+
+    enum telnet_state : uint8_t {
+        DATA = 0,
+        ESCAPED = 1,
+        SUBNEGOTIATION = 2,
+        IN_SUBNEGOTIATION = 3,
+        SUB_ESCAPED = 4,
+        COMMAND = 5,
+        ENDLINE = 6
+    };
+
+    class TelnetOptionHandler {
+
+    };
+
+
+
     class TelnetProtocol : public Protocol {
     public:
         explicit TelnetProtocol(TcpConnection *connection);
         void Receive(bytes data) override;
+    private:
+        void ProcessCommand(uint8_t command, uint8_t arg);
+        void ProcessSubCommand(uint8_t arg, bytes data);
+        void ProcessUserCommand(bytes data);
+        std::unordered_map<telnet_codes, std::shared_ptr<TelnetOptionHandler>> op_handlers;
+        uint8_t subcommand = 0;
+        telnet_codes command_mode = NUL;
+        telnet_state state = DATA;
+        bytes user_command_buffer;
+        bytes sub_command_buffer;
+        bool mccp2 = false;
+        bool mccp3 = false;
     };
 
     class TelnetProtocolHandler : public ProtocolHandler {
